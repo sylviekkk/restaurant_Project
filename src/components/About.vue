695af84e11cdaccfeda014bc9ac063fba16bb319
sylvie.kkk <sylvia.k.njagi@gmail.com>
2025-02-21 17:37:19 +0300

    updated project

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,71 @@
 <template>
-    <h1>About</h1>
+    <v-app-bar color="#c2d6f5" >
+        <h1>GLEM LINK</h1>
+    </v-app-bar>
+
+
+    <!--caroussel-->
+    <v-card class="mx-auto" elevation="24">
+
+
+<v-carousel>
+    <v-carousel-item src="/interior-shot-cafe-with-chairs-near-bar-with-wooden-tables.jpg" cover> </v-carousel-item>
+</v-carousel>
+
+</v-card>
+
+<v-container>
+        <v-card class="mx-auto" elevation="24" style="padding-left: 150px; padding-right: 10px;" >
+            <v-container>
+            <v-row>
+                <v-col cols="30" md="10">
+                    <v-card>
+                        <v-card color="#160b2b">
+                            <v-card-item>
+                                <v-card-title>
+                                    <H1>ABOUT US</H1> 
+                                </v-card-title>
+                                <v-card-text>
+                                   <p>Welcome to GemLink, where passion for exceptional food meets a warm, welcoming atmosphere. Founded in 2021, our mission is to create unforgettable dining experiences by offering delicious, locally-sourced dishes crafted with love and creativity.
+                                    At Gem link, we believe in the power of food to bring people together. Our menu is inspired by  flavors, with every dish prepared fresh to order using the finest ingredients. From our hand-selected produce to our carefully chosen meats, we focus on quality, sustainability, and supporting local farmers and suppliers.
+
+Our commitment to excellence doesn't just stop with food. We are dedicated to providing exceptional service, ensuring every guest feels like family from the moment they step through our doors. Whether you're here for a casual meal, a special celebration, or a romantic dinner, we aim to make every dining experience memorable.
+
+Join us for a meal, a drink, or simply to enjoy the atmosphere — we look forward to serving you!</p>
+ 
+                            </v-card-text>
+                            </v-card-item>
+                        </v-card>
+                    </v-card>
+                </v-col>
+            </v-row>
+            </v-container>
+        </v-card>
+</v-container>
 </template>
+
+<style>
+p{
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    text-align: center;
+    
+}
+
+H1{
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+h1{
+    font-weight:bolder;
+    text-align: center;
+    flex: auto;
+    font-family:Times New Roman
+}
+
+
+</style>
 
 <script >
 </script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -18,17 +18,50 @@
 
                                 <v-card-subtitle>{{ item.description }}</v-card-subtitle>
                             </v-card-item>
-                            <v-img height="200px" src="https://img.freepik.com/free-photo/grilled-chicken-thighs-pita-bread-served-with-fresh-vegetables_140725-251.jpg?t=st=1739955325~exp=1739958925~hmac=27e0265fec34e398c1013b99912d01b1f1ddb9c22e9296fef71b6b55ca85da04&w=740" cover ></v-img>
+                            <v-img height="200px" :src="item.image" cover ></v-img>
 
                             <v-card-text>
                            {{ item.price }}
                             </v-card-text>
+
+                         <v-btn color="black" variant="tonal" @click="addToOrder(item)">
+                           Order
+                        </v-btn>
                         </v-card>
+
                     </v-card>
                 </v-col>
             </v-row>
             </v-container>
     </v-card>
+    </v-container>
+
+    <v-container>
+        <v-card color="blue" >
+            <v-card-title>
+                <div class="text-h6">Current order: {{totalOrderAmount}}</div>
+            </v-card-title>
+        </v-card>
+    </v-container>
+    <v-container>
+        <v-card class="mx-auto" elevation="24">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" md="4" v-for="item in orderItems" :key=item>
+                        <v-card color="yellow">
+                            <v-card-item>
+                            <v-card-title>{{ item.name }}</v-card-title>
+
+                            <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+                            </v-card-item>
+                            <v-img height="200px" :src="item.image" cover ></v-img>
+
+                            <v-card-text>{{ item.price }}</v-card-text>
+                        </v-card>
+                        </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </v-container>
             
 </template>
@@ -41,15 +74,15 @@ const menuItems = [
         description: "Best chips in town",
         size: "Large",
         offers: "None",
-        image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+        image: "/crispy-french-fries-with-ketchup-mayonnaise.jpg"
     },
     {
-        name: "Sausage",
+        name: "Chicken Alfredo",
         price: 50,
-        description: "Best sausages in town",
+        description: "The only excusite Chicken Alfredo",
         size: "None",
         offers: "None",
-        image: "https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8"
+        image: "/freshness-healthy-eating-homemade-vegetarian-pasta-generated-by-artificial-intelligence.jpg"
     },
     {
         name:"Burger",
@@ -57,7 +90,39 @@ const menuItems = [
         description:"Flavoury chicken burger",
         size:"medium",
         offers: "Buy 2 get 1 FREE!",
-        image:"https://img.freepik.com/free-photo/close-up-three-mini-burgers-served-with-french-fries-paper-box_141793-2304.jpg?uid=R186032334&ga=GA1.1.1452505744.1738665279&semt=ais_hybrid"
+        image:"/burger-served-with-french-fries.jpg"
+    },
+    {
+        name:"Grilled fish fillet",
+        price:300,
+        description:"Savoury grilled Fish fillet with a side of home made tartar sauce",
+        size:"large",
+        offers:"None",
+        image: "/bass-fish-dish-assortment.jpg"
+    },
+    {
+        name:"Hawaian pizza",
+        price:350,
+        Description:"pizza savoured with pineapple",
+        size:"medium",
+        image:"/hawaiian-pizza-with-pineappleham-cheese-black-slate-background (1).jpg"
+    },
+    {
+        name:"Beef loaded fries"
     }
 ]
+
+//manage the current state of the order
+var orderItems=[]
+var totalOrderAmount=0
+
+function calculateTotalAmount(price){
+    totalOrderAmount += price
+}
+function addToOrder(item){
+    orderItems.push(item)
+    calculateTotalAmount(item.price)
+
+}
+
 </script>
